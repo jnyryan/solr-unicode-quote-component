7cf8c3c6996bea2e9a6e62e55c11dcae9526bbcc
John Ryan <jnyryan@gmail.com>
2019-01-24 12:36:05 +0000

    added component to handler

---
+++ README.md
@@ -0,0 +1,49 @@
+# Solr Unicode Quote Component
+
+## Usage
+
+Edit the `solrconfig.xml` of the core you want to enable the component for
+
+Add the following to that file
+
+The classpath to your component folder
+
+	``` xml
+	<lib dir="${solr.install.dir:../../../..}/dist/" regex="unicode-quote-component-\d.*\.jar" />
+	```
+	
+Register the custom component
+	``` xml
+	<searchComponent name="unicodeQuoteComponent" class="com.jnyryan.solr.components.FoldUnicodeQuotes"/>
+	```
+	
+In the request handler add the component as a first-component
+``` xml
+    <arr name="first-components">
+      <str>unicodeQuoteComponent</str>
+    </arr>
+```
+
+e.g.
+
+``` xml
+  <requestHandler name="/select" class="solr.SearchHandler">
+    <lst name="defaults">
+      <str name="echoParams">explicit</str>
+      <int name="rows">10</int>
+    </lst>
+    <arr name="first-components">
+      <str>unicodeQuoteComponent</str>
+      <!-- <str>unicodeQuoteComponentJar</str> -->
+    </arr>
+  </requestHandler>
+```
+
+
+##Test
+
+``` bash
+solr create_core -c testCore1 -d sample_techproducts_configs
+
+post -c testCore1 /usr/local/opt/solr-7.3.1/example/exampledocs/*
+```
---
+++ src/com/jnyryan/solr/components/FoldUnicodeQuotes.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jnyryan.solr.components;
+
+import java.io.IOException;
+import org.apache.solr.common.params.CommonParams;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.handler.component.ResponseBuilder;
+import org.apache.solr.handler.component.SearchComponent;
+import org.apache.solr.request.SolrQueryRequest;
+
+public class FoldUnicodeQuotes extends SearchComponent {
+
+  @Override
+  public void prepare(ResponseBuilder rb) throws IOException {
+    updateSolrRequest(rb.req);
+  }
+
+  @Override
+  public void process(ResponseBuilder rb) throws IOException {
+    System.out.println("I am processing inline");
+  }
+
+  @Override
+  public String getDescription() {
+    System.out.println("I am getting my description inline");
+    return null;
+  }
+  
+  /**
+   * Grab the Q and Q.ALT parameters and remove UNICODE quotes from them. 
+   * 
+   * @param req the request to the SOlr handler
+   */
+  public void updateSolrRequest(SolrQueryRequest req) {
+    SolrParams params = req.getParams();
+    String newQuery = replaceUnicodeDoubleQuotes(params.get(CommonParams.Q)).toString();
+    ModifiableSolrParams newParams = new ModifiableSolrParams(params);
+    newParams.remove(CommonParams.Q);
+    newParams.add(CommonParams.Q, newQuery);
+    req.setParams(newParams);
+  }
+  
+  /**
+   * Replace UNICODE double quotes with basic Latin standard quote mark (") - &#34; &quot;
+   * Replaces: 
+   *  “ left double quotation mark (%u201C)
+   *  ” right double quotation mark (%u201D)
+   * 〝 left curly quote (%u301D)
+   *  〞right curly quote (%u301E)
+   *  „ index quote german scandanavian (%u201E)
+   *  « left-pointing double angle quotation mark (%AB)
+   *  » right-pointing double angle quotation mark (%BB)
+   *  ‟ double high-reversed-9 quotation mark (%u201F)
+   *  ❝ heavy double turned comma quotation mark ornament (%u275D)
+   *  ❞ heavy double comma quotation mark ornament (%u275E)
+   *  ⹂ double low-reversed-9 quotation mark - (%u2E42)
+   *  ＂fullwidth quotation mark - (%uFF02)
+   *  
+   * @param s The query string
+   * @return Returns {@code s} with UNICODE double quotes replaced as standard double quote
+   */
+  public CharSequence replaceUnicodeDoubleQuotes(CharSequence s) {
+    return s.toString().replaceAll("[“”〝〞„«»‟❝❞⹂＂]","\"");
+  }
+
+}
